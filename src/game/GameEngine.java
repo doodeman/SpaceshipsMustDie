b1d8d9f3f1d2c3b60b155d09485cd5104393c70f
Kristleifur <batwigg@gmail.com>
2013-11-12 16:29:40 +0000

    First iteration of server.

---
+++ src/game/GameEngine.java
@@ -0,0 +1,33 @@
+package game;
+
+import java.util.List;
+
+import network.Client;
+import network.TCPServer;
+import network.UDPSender;
+import network.UDPServer;
+
+public class GameEngine 
+{
+	int port; 
+	UDPServer server; 
+	TCPServer tcpServer; 
+	
+	
+	List<Client> clients; 
+	GameState gameState; 
+	
+	/**
+	 * Sends gamestate update to all clients
+	 */
+	private void sendUpdate()
+	{
+		String updateString = gameState.toJson(); 
+		for (Client client : clients)
+		{
+			Runnable task = new UDPSender(client, port, updateString);
+			Thread worker = new Thread(task); 
+			worker.start(); 
+		}
+	}
+}
---
+++ src/network/NetworkTester.java
@@ -0,0 +1,20 @@
+package network;
+
+import java.io.IOException;
+
+import game.GameEngine;
+import game.GameState;
+
+public class NetworkTester 
+{
+	public static void main(String arg[]) throws IOException
+	{
+		TCPServer server = new TCPServer(1234, new GameState(), new GameEngine());
+		Thread serverWorker = new Thread(server); 
+		serverWorker.start();
+		
+		ClientTCPClient client = new ClientTCPClient("localhost", 1234); 
+		Thread clientWorker = new Thread(client); 
+		clientWorker.start();
+	}
+}
