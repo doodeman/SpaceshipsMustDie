badf17108fc660075af573f50fb3ea8f247d5e66
Kristleifur <batwigg@gmail.com>
2013-11-15 15:44:37 +0000

    Adding client controller

---
+++ src/client/ClientController.java
@@ -0,0 +1,39 @@
+package client;
+
+public class ClientController 
+{
+	public void forward()
+	{
+		
+	}
+	
+	public void backward()
+	{
+		
+	}
+	
+	public void left()
+	{
+		
+	}
+	
+	public void right()
+	{
+		
+	}
+	
+	public void fire()
+	{
+		
+	}
+	
+	public void up()
+	{
+		
+	}
+	
+	public void down()
+	{
+		
+	}
+}
